3b8aa55f13f66605ce7eddc7128cf8c63bd1acb7
Pixum <pixum2001@gmail.com>
2024-04-14 15:32:35 +0200

    Light-Dark Theme Toggle

--- main.js
+++ main.js
@@ -19,3 +19,46 @@ function addAnimation() {
         })
     });
 }
+
+const root = document.documentElement;
+const themeSwitch = document.getElementById("theme-switch");
+const themeIcon = document.getElementById("theme-icon");
+
+const lightIconName = 'fa-solid fa-sun';
+const darkIconName = 'fa-solid fa-moon';
+
+window.addEventListener('DOMContentLoaded', initializeTheme);
+themeSwitch.onclick = function () { switchTheme() };
+
+function switchTheme() {
+    // If the site has just been loaded
+    if (root.getAttribute('data-theme') === null) {
+        initializeTheme();
+        return;
+    }
+
+    setTheme(root.getAttribute('data-theme') === 'light' ? 'dark' : 'light');
+}
+
+function initializeTheme() {
+    if (localStorage.getItem('preferredTheme') === null) {
+        const usePreferredSettings = false;
+        var prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+        if (usePreferredSettings) {
+            setTheme(prefersDarkMode ? 'dark' : 'light');
+        }
+        else {
+            setTheme('light');
+        }
+        return;
+    }
+
+    setTheme(localStorage.getItem('preferredTheme'));
+}
+
+function setTheme(theme) {
+    root.setAttribute('data-theme', theme);
+    localStorage.setItem('preferredTheme', theme);
+    themeIcon.className = theme === 'light' ? darkIconName : lightIconName;
+}
